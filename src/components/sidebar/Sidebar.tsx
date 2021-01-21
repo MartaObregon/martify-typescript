41fd3ad85db95d9fd18f8a5321bdf68123851676
Marta Obregon <marta.garcia.obregon@.com>
2021-01-21 17:15:13 +0000

    sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styled from 'styled-components';
 import { GlobalTheme } from '../../styles/theme';
-import MenuOption from './MenuOption';
+
+
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
 import SearchRoundedIcon from '@material-ui/icons/SearchRounded';
-import ListIcon from '@material-ui/icons/List';
-
+import VideoLibraryRoundedIcon from '@material-ui/icons/VideoLibraryRounded';
 
 const SidebarContainer = styled.div`
   background: ${GlobalTheme.colors.grey};
@@ -18,6 +18,7 @@ const SidebarContainer = styled.div`
   
 `;
 
+
 const Logo = styled.img`
   max-width:150px;
   height:44px
@@ -25,7 +26,14 @@ const Logo = styled.img`
 
 
 const Menu = styled.div`
-  /* ... */
+  margin-left:10px;
+`;
+
+const MenuOption = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: flex-end;
+  align-items: center;
 `;
 
 
@@ -36,9 +44,18 @@ function Sidebar() {
         <SidebarContainer>
             <Logo alt="" src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"/>
             <Menu>
-               <MenuOption title = "Home" Icon = {HomeRoundedIcon}/>
-               <MenuOption title = "Explore" Icon = {SearchRoundedIcon}/>
-               <MenuOption title = "Library" Icon = {ListIcon}/>
+               <MenuOption>
+                   <h3>Home</h3>
+                   <HomeRoundedIcon/>
+               </MenuOption>
+               <MenuOption>
+                   <h3>Explore</h3>
+                   <SearchRoundedIcon/>
+               </MenuOption>
+               <MenuOption>
+                   <h3>Library</h3>
+                   <VideoLibraryRoundedIcon/>
+               </MenuOption>
             </Menu>
             {/* playlists */}
         </SidebarContainer>
